1fa2e6d8e1d3d1e4526f81c3c7bed76347d30aa4
dandeliao <danielramos@tuta.io>
2021-11-01 12:39:25 -0300

    fix: edita dados (pessoas, comunidades, etc)

--- index.js
+++ index.js
@@ -20,10 +20,270 @@ const pessoas = [
         id: 0,
         nome: "dani",
         comunidades: ["radio-maloca", "filmes"],
-        html: "<h1 style='font-size:32px'>Oi! Sou Dani :)</h1><marquee>bem-vinde à minha página pessoal</marquee>"
+        html: `
+        <m-container style="
+            --background-image: url('https://upload.wikimedia.org/wikipedia/commons/8/88/Tile_n_728.png');
+            --container-background: rgba(256, 256, 256, 0.3);
+            ">
+            <m-card>
+                <div class="info-perfil">
+                    <img src="http://localhost:4000/pessoas/dani/avatar.png">
+                    <div>
+                        <h1>Oi! Sou Dani :)</h1>
+                        <br>
+                        <ul>
+                            <li>desde 1990</li>
+                            <li>70% algodão</li>
+                            <li>feito pra durar</li>
+                            <li>às vezes dá tilt</li>
+                        </ul>
+                    </div>
+                </div>
+                <marquee>bem-vinde à minha página pessoal</marquee>
+            </m-card>
+            <m-card>
+                <h3>Aqui posso colocar comunidades que gosto, links, fotos...</h3>
+                <p>(...)</p>
+            </m-card>
+            <m-card>
+                <p>posso inserir quantos cards quiser :B</p>
+                <br>
+                <button title="javascript não pode :X só executa o que já vem embutido em elementos como o card">injetar script</button>
+            </m-card>
+        </m-container>
+        <style>
+            .info-perfil {
+                display: flex;
+                flex-direction: row;
+                justify-content: start;
+            }
+            .info-perfil > img {
+                max-width: 200px;
+                width: 100%;
+                height: auto;
+                border-radius: 100%;
+                margin-right: 2rem;
+            }
+            ul {
+                margin-left: 2rem;
+            }
+        </style>
+        `
+    },
+    {
+        id: 1,
+        nome: "rukib",
+        comunidades: [],
+        html: `
+        <m-container style="
+            --background-image: url('https://upload.wikimedia.org/wikipedia/commons/c/c0/Gray_background.gif');
+            --container-background: transparent;
+            ">
+            <m-card>
+                <div class="info-perfil">
+                    <img src="http://localhost:4000/pessoas/rukib/avatar.png">
+                    <div>
+                        <h1>rukib</h1>
+                        <marquee>i am am i who am i</marquee>
+                    </div>
+                </div>
+            </m-card>
+            <m-card>
+                <h3>Aqui posso colocar comunidades que gosto, links, fotos...</h3>
+                <p>(...)</p>
+            </m-card>
+            <div id="cardao">
+                <m-card class="cardim">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Flower_market_cross_stitch%2C_v1.jpg/613px-Flower_market_cross_stitch%2C_v1.jpg">
+                </m-card>
+                <m-card class="cardim">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Reiher_im_Starkwind.jpg/639px-Reiher_im_Starkwind.jpg">
+                </m-card>
+                <m-card class="cardim">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Sancerre%2C_Rue_Saint-Jean.jpg/583px-Sancerre%2C_Rue_Saint-Jean.jpg">
+                </m-card>
+                <m-card class="cardim">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Ceramicagrottagliese.jpg/640px-Ceramicagrottagliese.jpg">
+                </m-card>
+                <m-card class="cardim">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Bienenweide.jpg/568px-Bienenweide.jpg">
+                </m-card>
+                <m-card class="cardim">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Northsea-eckwarderh%C3%B6rne_hg.jpg/640px-Northsea-eckwarderh%C3%B6rne_hg.jpg">
+                </m-card>
+                <m-card class="cardim">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Kumpulanpuro_culvert.jpg/511px-Kumpulanpuro_culvert.jpg">
+                </m-card>
+                <m-card class="cardim">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Mt._Ararat_via_Khor_Virap%2C_Armenia.jpg/640px-Mt._Ararat_via_Khor_Virap%2C_Armenia.jpg">
+                </m-card>
+            </div>
+        </m-container>
+        <style>
+            .info-perfil {
+                display: flex;
+                flex-direction: row;
+                justify-content: start;
+            }
+            .info-perfil > img {
+                max-width: 200px;
+                width: 100%;
+                height: auto;
+                border-radius: 100%;
+                margin-right: 2rem;
+            }
+            #cardao {
+                display: flex;
+                flex-direction: row;
+                flex-wrap: wrap;
+                justify-content: space-around;
+                gap: 1em;
+            }
+            .cardim {
+                flex-grow: 0;
+            }
+            .cardim img {
+                max-width: 250px;
+                height: auto;
+            }
+        </style>
+        `
+    }
+];
+const comunidades = [
+    {
+        id: 0,
+        nome: "radio-maloca",
+        html: `
+        <m-container style="
+                --background-image: url('https://upload.wikimedia.org/wikipedia/commons/5/5e/Tile_n_745.png');
+                --container-background: rgba(170, 170, 170, 0.8);
+            ">
+            <m-card>
+                <div class="info">
+                    <img src='http://localhost:4000/comunidades/radio-maloca/avatar.png' />
+                    <div>
+                        <h1>Rádio Maloca</h1>
+                        <p>comunidade para ouvir e compartilhar música</p>
+                        <br>
+                        <p>24h no ar</p>
+                    </div>
+                </div>
+            </m-card>
+            <m-card>
+                <h3>Rap</h3>
+                <p>(...)</p>
+            </m-card>
+            <m-card>
+                <h3>Sol e praia</h3>
+                <p>(...)</p>
+            </m-card>
+            <m-card>
+                <h3>Para estudar/relaxar</h3>
+                <p>(...)</p>
+            </m-card>
+            <m-card>
+                <h3>Agito</h3>
+                <p>(...)</p>
+            </m-card>
+        </m-container>
+        <style>
+            .info {
+                display: flex;
+                flex-direction: row;
+                justify-content: start;
+            }
+            .info > img {
+                max-width: 200px;
+                width: 100%;
+                height: auto;
+                border-radius: 100%;
+                margin-right: 2rem;
+            }
+            ul {
+                margin-left: 2rem;
+            }
+        </style>
+
+        `
     }
 ];
 
+const instancias = [
+    {
+        id: 0,
+        nome: "maloca",
+        html: `
+        <m-container style="
+            --background-image: linear-gradient(lightpink, #a7f3d0);
+            --container-background: rgba(256, 256, 256, 0.3);
+            ">
+            <m-card style="
+                --box-shadow: none;
+                --background: transparent;
+                ">
+                <div class="info">
+                    <div>
+                        <h1>Oi, maloqueire!</h1>
+                        <p>seja bem-vinde à nossa cabana comunitária</p>
+                    </div>
+                </div>
+            </m-card>
+            <m-card style="
+                --background: khaki;
+                --border-radius: 0;
+                --border-style: dotted;
+                --box-shadow: none;
+                ">
+                <h3>Mural de avisos</h3>
+                <p>---</p>
+            </m-card>
+            <div id="cardao">
+                <m-card class="cardim">
+                    <h3>Comunidades</h3>
+                    <p>(...)</p>
+                </m-card>
+                <m-card class="cardim">
+                    <h3>Chat</h3>
+                    <p>(...)</p>
+                </m-card>
+            </div>
+        </m-container>
+        <style>
+            .info {
+                display: flex;
+                flex-direction: row;
+                justify-content: start;
+            }
+            .info > img {
+                max-width: 200px;
+                width: 100%;
+                height: auto;
+                border-radius: 100%;
+                margin-right: 2rem;
+            }
+            #cardao {
+                display: flex;
+                flex-direction: row;
+                flex-wrap: wrap;
+                justify-content: space-around;
+                gap: 1em;
+            }
+            .cardim {
+                flex-grow: 1;
+            }
+        </style>
+        `
+    }
+];
+
+const pecinhas = [
+    {
+        id: 0,
+        nome: "m-card",
+        js: './pecinhas/m-card.js'
+    }
+];
 
 // routing
 // ---
