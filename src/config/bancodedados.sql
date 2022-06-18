7a413add5dde10fe6412fde66c89293839f5d4de
dandeliao <dandeliao@tutanota.com>
2022-06-18 00:45:48 -0300

    fix: campos das tabelas pessoa/autent. no bd

--- src/config/bancodedados.sql
+++ src/config/bancodedados.sql
@@ -6,19 +6,19 @@ CREATE TABLE pessoas(
     nome            VARCHAR(64) NOT NULL,
     avatar          VARCHAR(255) DEFAULT 'avatar.jpg',
     fundo           VARCHAR(255) DEFAULT 'fundo.jpg',
-    email           VARCHAR UNIQUE NOT NULL,
     data_ingresso   TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     indicada_por    uuid REFERENCES pessoas(pessoa_id)
 );
 
-CREATE TABLE senhas(
+CREATE TABLE autenticacao(
     pessoa_id       uuid PRIMARY KEY REFERENCES pessoas(pessoa_id),
+    email           VARCHAR UNIQUE NOT NULL,
     hash            VARCHAR NOT NULL,
     salt            VARCHAR NOT NULL
 );
 
 CREATE TABLE paginas_pessoais(
-    pagina_pessoal_id   BIGSERIAL PRIMARY KEY,
+    pagina_pessoal_id   uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     pessoa_id           uuid REFERENCES pessoas(pessoa_id),
     arquivo_html        VARCHAR(255),
     nome                VARCHAR(64),
