a5e09a03069da716a13a4b223aa7927e3b10f7f2
dandeliao <dandeliao@tutanota.com>
2022-06-19 15:15:23 -0300

    maint: altera arquivo de configuracao do db

--- src/config/create.sql
+++ src/config/create.sql
@@ -1,27 +1,27 @@
 CREATE DATABASE maloca
 
 CREATE TABLE pessoas(
-    pessoa_id       uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    arroba          VARCHAR(32) UNIQUE NOT NULL,
+    pessoa_id       VARCHAR(32) PRIMARY KEY NOT NULL,
     nome            VARCHAR(64) NOT NULL,
     avatar          VARCHAR(255) DEFAULT 'avatar.jpg',
     fundo           VARCHAR(255) DEFAULT 'fundo.jpg',
     data_ingresso   TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    indicada_por    uuid REFERENCES pessoas(pessoa_id)
+    indicada_por    VARCHAR(32) REFERENCES pessoas(pessoa_id)
 );
 
 CREATE TABLE autenticacao(
-    pessoa_id       uuid PRIMARY KEY REFERENCES pessoas(pessoa_id),
+    pessoa_id       VARCHAR(32) PRIMARY KEY REFERENCES pessoas(pessoa_id),
     email           VARCHAR UNIQUE NOT NULL,
     hash            VARCHAR NOT NULL,
     salt            VARCHAR NOT NULL
 );
 
 CREATE TABLE paginas_pessoais(
-    pagina_pessoal_id   uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    pessoa_id           uuid REFERENCES pessoas(pessoa_id),
+    pessoa_id           VARCHAR(32) REFERENCES pessoas(pessoa_id),
+    pagina_pessoal_id   BIGSERIAL,
     arquivo_html        VARCHAR(255),
     nome                VARCHAR(64),
     publica             BOOLEAN DEFAULT false,
-    criacao             TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+    criacao             TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(pessoa_id, pagina_pessoal_id)
 );
