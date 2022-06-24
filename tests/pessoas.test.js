d5a56c3a919f78cdd79627ec05eb1c6f78ba8faf
dandeliao <danielramos@tuta.io>
2022-06-24 12:16:41 -0300

    testa erros 201, 204, 404 e 409

--- tests/pessoas.test.js
+++ tests/pessoas.test.js
@@ -3,18 +3,26 @@ const axios = require('axios');
 const servicePessoas = require('../src/api/v1/services/servicePessoas');
 const geraString = require('../src/api/v1/utils/utilGenString');
 
+// ---
+// auxiliares
+
 function geraPessoa() {
 	let pessoa_id = geraString(1, 32, 'simples');
 	let nome = geraString(1, 64, 'completo');
 	return { pessoa_id, nome };
 }
 
-async function request(caminho, method, data) {
+function request(caminho, method, data) {
 	let url = `http://localhost:4000${caminho}`;
-	return await axios({ url, method, data });
+	return axios({ url, method, data, validateStatus: false});
 }
 
-test('esperado: obtém (get) pessoas', async () => {
+// ---
+// testes
+
+// rotas get
+
+test('esperado: obtém pessoas', async () => {
 
 	const pessoa1 = geraPessoa();
 	const pessoa2 = geraPessoa();
@@ -26,6 +34,7 @@ test('esperado: obtém (get) pessoas', async () => {
 	const response = await request('/pessoas', 'get');
 	const pessoas = response.data;
 
+	expect(response.status).toBe(200);
 	expect(pessoas).toHaveLength(3);
 	await servicePessoas.deletePessoa(p1.pessoa_id);
 	await servicePessoas.deletePessoa(p2.pessoa_id);
@@ -33,47 +42,95 @@ test('esperado: obtém (get) pessoas', async () => {
 
 });
 
-test('esperado: obtém (get) pessoa específica', async () => {
+test('esperado: obtém pessoa específica', async () => {
 
-	const pessoa1 = geraPessoa();
-	const p = await servicePessoas.postPessoa(pessoa1);
+	const dados = geraPessoa();
+	const p = await servicePessoas.postPessoa(dados);
 
 	const response = await request(`/pessoas/${p.pessoa_id}`, 'get');
 	const pessoa = response.data;
 
+	expect(response.status).toBe(200);
 	expect(pessoa.pessoa_id).toBe(p.pessoa_id);
 	await servicePessoas.deletePessoa(p.pessoa_id);
 
 });
 
-test('esperado: cria (post) nova pessoa', async () => {
+test('esperado: não obtém pessoa específica (404)', async () => {
+
+	const pessoa = geraPessoa();
+
+	const response = await request(`/pessoas/${pessoa.pessoa_id}`, 'get');
+
+	expect(response.status).toBe(404);
+
+});
+
+// rotas post
+
+test('esperado: cria nova pessoa', async () => {
 
 	const dados = geraPessoa();
 	
-	const responsePost = await request('/pessoas', 'post', dados);
-	const { pessoa_id } = responsePost.data;
+	const response = await request('/pessoas', 'post', dados);
+	const pessoa = response.data;
 
-	const responseGet = await request(`/pessoas/${pessoa_id}`, 'get');
-	const pessoa = responseGet.data;
+	expect(response.status).toBe(201);
 	expect(pessoa.pessoa_id).toBe(dados.pessoa_id);
 	expect(pessoa.nome).toBe(dados.nome);
 	await servicePessoas.deletePessoa(pessoa.pessoa_id);
 
 });
 
-test('esperado: modifica (put) pessoa', async () => {
+test('esperado: não cria nova pessoa (409: conflito)', async () => {
+
+	const dados = geraPessoa();
+	
+	await request('/pessoas', 'post', dados);
+	const response = await request('/pessoas', 'post', dados);
+
+	expect(response.status).toBe(409);
+	await servicePessoas.deletePessoa(dados.pessoa_id);
+
+});
+
+// rotas put
+
+test('esperado: modifica pessoa', async () => {
+
+	/*
+		falta testar put de avatar e fundo (como integrar conteúdo estático?)
+
+		restringir/proibir put de pessoa_id 
+		proibir put de data_criacao
+
+	*/
+
 	const pessoa = geraPessoa();
 	const p = await servicePessoas.postPessoa(pessoa);
 	
 	const novaPessoa = geraPessoa();
-	await request(`/pessoas/${p.pessoa_id}`, 'put', novaPessoa);
+	const response = await request(`/pessoas/${p.pessoa_id}`, 'put', novaPessoa);
 	
 	const pessoaAtualizada = await servicePessoas.getPessoa(novaPessoa.pessoa_id);
+	expect(response.status).toBe(204);
 	expect(pessoaAtualizada.pessoa_id).toBe(novaPessoa.pessoa_id);
 	expect(pessoaAtualizada.nome).toBe(novaPessoa.nome);
 	await servicePessoas.deletePessoa(pessoaAtualizada.pessoa_id);
 
 });
+
+test('esperado: não modifica pessoa (404)', async () => {
+
+	const pessoa = geraPessoa();
+	
+	const response = await request(`/pessoas/${pessoa.pessoa_id}`, 'put', pessoa);
+	
+	expect(response.status).toBe(404);
+
+});
+
+// rotas delete
 
 test('esperado: deleta pessoa', async () => {
 
@@ -81,8 +138,19 @@ test('esperado: deleta pessoa', async () => {
 	const { pessoa_id } = await servicePessoas.postPessoa(pessoa);
 
 	const response = await request(`/pessoas/${pessoa_id}`, 'delete');
+	
 	const pessoas = await servicePessoas.getPessoas();
 	expect(pessoas).toHaveLength(0);
-	expect(response.data).toBe('sucesso');
+	expect(response.status).toBe(204);
 	
+});
+
+test('esperado: não deleta pessoa (404)', async () => {
+
+	const pessoa = geraPessoa();
+
+	const response = await request(`/pessoas/${pessoa.pessoa_id}`, 'delete');
+
+	expect(response.status).toBe(404);
+
 });
