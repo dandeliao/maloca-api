861d4cf14eb644729d5272988efb1e01ee5af8a5
dandeliao <danielramos@tuta.io>
2022-06-25 12:57:22 -0300

    renomeia camada de acesso ao banco de dados

--- src/api/v1/services/servicePessoas.js
+++ src/api/v1/services/servicePessoas.js
@@ -1,31 +1,31 @@
-const controllerPessoas = require('../controllers/controllerPessoas');
+const dataPessoas = require('../data/dataPessoas');
 
 exports.getPessoas = async function () {
-	const objetoPessoas = await controllerPessoas.getPessoas();
+	const objetoPessoas = await dataPessoas.getPessoas();
 	return objetoPessoas.rows;
 };
 
 exports.getPessoa = async function (pessoaId) {
-	const objetoPessoa = await controllerPessoas.getPessoa(pessoaId);
+	const objetoPessoa = await dataPessoas.getPessoa(pessoaId);
 	if (objetoPessoa.rowCount === 0) throw new Error ('pessoa não encontrada');
 	return objetoPessoa.rows[0];
 };
 
 exports.postPessoa = async function (pessoa) {
-	const pessoaExistente = await controllerPessoas.getPessoa(pessoa.pessoa_id);
+	const pessoaExistente = await dataPessoas.getPessoa(pessoa.pessoa_id);
 	if (pessoaExistente.rowCount !== 0) throw new Error('pessoa já existe');
-	const objetoPessoaId = await controllerPessoas.postPessoa(pessoa);
+	const objetoPessoaId = await dataPessoas.postPessoa(pessoa);
 	return objetoPessoaId.rows[0];
 };
 
 exports.putPessoa = async function (pessoaId, pessoa) {
-	const objetoPessoa = await controllerPessoas.putPessoa(pessoaId, pessoa);
+	const objetoPessoa = await dataPessoas.putPessoa(pessoaId, pessoa);
 	if (objetoPessoa.rowCount === 0) throw new Error('pessoa não encontrada');
 	return objetoPessoa.rows[0];
 };
 
 exports.deletePessoa = async function (pessoaId) {
-	const veredito = await controllerPessoas.deletePessoa(pessoaId);
+	const veredito = await dataPessoas.deletePessoa(pessoaId);
 	if (veredito.rowCount === 0) throw new Error('pessoa não encontrada');
 	return veredito.rowCount;
 };
