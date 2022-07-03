677a1095e8b7bce779a0da54bd4087f92ebf8ee9
dandeliao <danielramos@tuta.io>
2022-07-03 11:13:08 -0300

    corrrige comportamento do error handler

--- src/api/v1/middlewares/errorHandler.js
+++ src/api/v1/middlewares/errorHandler.js
@@ -1,14 +1,11 @@
 function errorHandler (erro, req, res, next) {
 	if (erro.message === 'pessoa não encontrada') {
-		res.status(404).send(erro.message);
-		next();
+		return res.status(404).send(erro.message);
 	}
 	if (erro.message === 'pessoa já existe') {
-		res.status(409).send(erro.message);
-		next();
+		return res.status(409).send(erro.message);
 	}
 	res.status(500).send(erro.message);
-	next();
 }
 
 module.exports = errorHandler;
