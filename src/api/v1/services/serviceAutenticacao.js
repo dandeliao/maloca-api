7a0b595b12e28d04b18dcd9685442a9238adcebf
dandeliao <danielramos@tuta.io>
2022-10-06 11:59:49 -0300

    cria pasta pessoal ao registrar pessoa

--- src/api/v1/services/serviceAutenticacao.js
+++ src/api/v1/services/serviceAutenticacao.js
@@ -1,6 +1,8 @@
 const dataPessoas = require('../data/dataPessoas');
 const dataAutenticacao = require('../data/dataAutenticacao');
 const geraHashESalt = require('../../../utils/utilPassword').geraHashESalt;
+const fs = require('fs');
+const path = require('path');
 
 exports.postRegistro = async function (dados) {
 	const pessoaExistente = await dataPessoas.getPessoa(dados.pessoa_id);
@@ -17,5 +19,15 @@ exports.postRegistro = async function (dados) {
 	};
 	const arrayNovaPessoa = await dataPessoas.postPessoa(pessoa);
 	await dataAutenticacao.postSegredos(arrayNovaPessoa.rows[0].pessoa_id, segredos);
+	const pastaPessoal = path.join(path.resolve(__dirname, '../../../../static'), 'pessoas', `${dados.pessoa_id}`);
+	if (!fs.existsSync(pastaPessoal)){
+		fs.mkdirSync(pastaPessoal);
+	}
+	if (!fs.existsSync(path.join(pastaPessoal, 'imagens'))){
+		fs.mkdirSync(path.join(pastaPessoal, 'imagens'));
+	}
+	if (!fs.existsSync(path.join(pastaPessoal, 'paginas'))){
+		fs.mkdirSync(path.join(pastaPessoal, 'paginas'));
+	}
 	return arrayNovaPessoa.rows[0];
 };
